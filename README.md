ab535ef4b16ac384b7e18aa63c6d2094feb7eefa
Jake Worth <jakeworth82@gmail.com>
2015-02-17 22:46:09 -0600

    Explore preload, eager_load, and includes

--- README.md
+++ README.md
@@ -4,9 +4,15 @@ This is a simple educational app to help explain and diagnose the N + 1 problem.
 
 By its very nature, it is not an example of exemplary code.
 
-Setup:
+### Setup
+
 ```
 git clone https://github.com/jwworth/n-plus
 cd n-plus
 rake db:migrate db:populate
 ```
+
+### Eager Loading
+
+The `preload`, `eager_load`, and `includes` methods are explored in the users controller.
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,33 @@
 class UsersController < ApplicationController
   def index
+    # Return all users
+    # Typical result: (Views: 1495.8ms | ActiveRecord: 2273.4ms)
+    #
     @users = User.all
+
+    # Return all users with comments preloaded
+    # Typical result: (Views: 1186.8ms | ActiveRecord: 1592.0ms)
+    #
+    # @users = User.all.preload(:comments)
+
+    # Return all users with comments and reviews preloaded
+    # Typical result: (Views: 678.8ms | ActiveRecord: 30.0ms)
+    #
+    # @users = User.all.preload(:comments, :reviews)
+
+    # Return all users with comments eager loaded
+    # Typical result: (Views: 1135.6ms | ActiveRecord: 1534.2ms)
+    #
+    # @users = User.all.eager_load(:comments)
+
+    # Return all users with comments and reviews eager loaded
+    # Typical result: (Views: 1409.5ms | ActiveRecord: 611.5ms)
+    #
+    # @users = User.all.eager_load(:comments, :reviews)
+
+    # Return all users with comments and reviews included
+    # Typical result: (Views: 870.2ms | ActiveRecord: 25.7ms)
+    #
+    # @users = User.all.includes(:comments, :reviews)
   end
 end
