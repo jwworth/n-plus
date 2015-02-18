1209faa631f9c8dfd31d07dcb312691a30577f0f
Jake Worth <jakeworth82@gmail.com>
2015-02-17 23:00:49 -0600

    Fix Rails boilerplate after an app name change

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_show_and_tell_02182015_session'
+Rails.application.config.session_store :cookie_store, key: 'n-plus_session'
